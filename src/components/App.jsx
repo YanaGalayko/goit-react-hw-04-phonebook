cb7c015976cc83f41b48b559bca9c5108dd94b4f
YanaGalayko <117485236+YanaGalayko@users.noreply.github.com>
2023-09-11 12:24:42 +0200

    hooks refactoring App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { useState, useEffect } from "react";
 import { nanoid } from 'nanoid'
 import { ContactForm } from "./ContactForm/ContactForm";
 import { ContactFilter } from "./ContactFilter/ContactFilter";
@@ -6,90 +6,78 @@ import { ContactList } from "./ContactList/ContactList";
 import { Layout } from "./Layout";
 import { GlobalStyle } from "./GlobalStyle";
 
-export class App extends Component {
-  state = {
-    contacts: [],
-    filter: ''
+const getIntialContacts = () => {
+  const savedContacts = localStorage.getItem('key')
+  if(savedContacts !== null) {
+    return JSON.parse(savedContacts)
   }
+  return []
+}
 
-  componentDidMount() {
-    const savedContacts = localStorage.getItem('key')
+export const App = () => {
+  const [contacts, setContacts] = useState(getIntialContacts)
+  const [filter, setFilter] = useState('')
 
-    if(savedContacts !== null) {
-      this.setState({ contacts: JSON.parse(savedContacts)})
-    }
-  }
-
-  componentDidUpdate(_, prevState) {
-  const { contacts } = this.state
-
-  if(prevState.contacts !== contacts) {
+  useEffect(() => {
     localStorage.setItem('key', JSON.stringify(contacts))
-  }
-  }
+  }, [contacts])
 
-  addContact = newContact => {
-    const isContactName = this.state.contacts.some(
+
+  const addContact = (newContact) => {
+    const isContactName = contacts.some(
       contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
     );
-
-    const isContactNumber = this.state.contacts.some(
-      contact => contact.number === newContact.number
-    );
-
+    
     if (isContactName) {
       alert(`${newContact.name} is already in contacts.`);
       return;
     }
+
+    const isContactNumber = contacts.some(
+      contact => contact.number === newContact.number
+    );
 
     if (isContactNumber) {
       alert(`${newContact.number} is already in contacts.`);
       return;
     }
 
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, { id: nanoid(), ...newContact }],
-    }));
+    setContacts(prevContacts => ([
+      ...prevContacts,
+      { id: nanoid(), ...newContact },
+    ]))
   };
 
-  deleteContact = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-    }));
+  const deleteContact = (contactId) => {
+    setContacts(prevContacts => prevContacts.filter(contact => contact.id !== contactId),
+    );
   };
 
-  filterContact = newFilter => {
-    this.setState({
-      filter: newFilter,
-    });
+  const filterContact = (newFilter) => {
+    setFilter(newFilter);
   };
-
-render() {
-  const { contacts, filter } = this.state;
 
   const filteredContacts = contacts.filter(contact =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
+    contact.name.toLowerCase().includes(filter.toLowerCase()))
+
 
   return (
     <Layout>
       <h1>Phonebook</h1>
 
-          <ContactForm onAdd={this.addContact}/>
+          <ContactForm onAdd={addContact}/>
 
       <h2>Contacts</h2>
        
       {contacts.length > 0 ? (
         <>
-      <ContactFilter filter={filter} onChangeFilter={this.filterContact}/>
+      <ContactFilter filter={filter} onChangeFilter={filterContact}/>
       <ContactList
         contacts={filteredContacts}
-        onDelete={this.deleteContact}/> 
+        onDelete={deleteContact}/> 
         </>)
         : (<p>Contact list is empty, please add contacts!</p>)} 
         <GlobalStyle/>  
     </Layout>
   )
-}
- 
 }
